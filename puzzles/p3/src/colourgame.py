5f45858b590b503d88d34d4508204397e4043da6
Mike Fulton <mikefultonpersonal@gmail.com>
2023-12-02 13:32:23 -0500

    p3 done

--- puzzles/p3/src/colourgame.py
+++ puzzles/p3/src/colourgame.py
@@ -7,9 +7,33 @@ GMAX = 13
 BMAX = 14
 
 def game_results(line):
-    possible=True
-    gid=1
-    return possible,gid
+    if line.strip() == '':
+        return False, 0
+
+    game_info = line.strip().split(':')
+    game_el = game_info[0].split(' ')
+    game_id = int(game_el[1])
+
+    reveals = game_info[1].split(';')
+
+    for reveal in reveals:
+        red_tot = green_tot = blue_tot = 0
+        grabs = reveal.split(',')
+        for grab in grabs:
+            tok = grab.strip().split(' ')
+            num = int(tok[0])
+            colour = tok[1]
+            match colour:
+                case 'red':
+                    red_tot += num
+                case 'green':
+                    green_tot += num
+                case 'blue':
+                    blue_tot += num
+        if red_tot > RMAX or green_tot > GMAX or blue_tot > BMAX:
+            return False, game_id
+
+    return True, game_id
 
 def main():
     if len(sys.argv) < 2:
@@ -24,11 +48,11 @@ def main():
         sys.stderr.write("Unable to open: " + input_file + " for read\n")
         return 8
 
-    Lines = colour_file.readlines()
+    lines = colour_file.readlines()
     
     tgid = 0
-    for line in Lines:
-        possible,gid = game_results(line)
+    for line in lines:
+        possible, gid = game_results(line)
         if possible:
             tgid += gid
 
