26a8352d3722123ee3328bcce99b227333c506c1
Mike Fulton <mikefultondev@gmail.com>
2023-12-06 23:03:59 -0500

    basic read of numbers into D and T arrays

--- puzzles/p11/src/race.cbl
+++ puzzles/p11/src/race.cbl
@@ -16,23 +16,42 @@
         WORKING-STORAGE SECTION.
          01    IN-EOF        PIC A(1) VALUE "N".
          01    FILE-STATUS  PIC X(2).
+         01    TME         PIC X(10).
+         01    DISTANCE     PIC X(10).
+         01    TIME-TABLE.
+           05    T          PIC X(4) OCCURS 4 TIMES.
+         01    DISTANCE-TABLE.
+           05    D          PIC X(4) OCCURS 4 TIMEs.
+         01    I            PIC 9.
 
         PROCEDURE DIVISION.
 
           MAIN.
             OPEN INPUT INPUT-FILE.
-            PERFORM READ-LINE THRU READ-LINE-EXIT UNTIL IN-EOF = "Y".
+            PERFORM READ-TIME.
+            PERFORM READ-DISTANCE.
             CLOSE INPUT-FILE.
             STOP RUN.
-          MAIN-EXIT.
             EXIT.
 
-          READ-LINE.
+          READ-TIME.
             READ INPUT-FILE
-              AT END
-                MOVE "Y" TO IN-EOF
               NOT AT END
-                DISPLAY '*******', INREC
-            END-READ.
-          READ-LINE-EXIT.
-            EXIT.
+                UNSTRING INREC DELIMITED BY ALL SPACE
+                  INTO TME T(1) T(2) T(3) T(4)
+                END-UNSTRING.
+            DISPLAY '--' TME '--'.
+            PERFORM VARYING I FROM 1 BY 1 UNTIL I = 4
+              DISPLAY T(I)
+            END-PERFORM.
+
+          READ-DISTANCE.
+            READ INPUT-FILE
+              NOT AT END
+                UNSTRING INREC DELIMITED BY ALL SPACE
+                  INTO DISTANCE D(1) D(2) D(3) D(4)
+                END-UNSTRING.
+            DISPLAY '--' DISTANCE.
+            PERFORM VARYING I FROM 1 BY 1 UNTIL I = 4
+              DISPLAY D(I)
+            END-PERFORM.
