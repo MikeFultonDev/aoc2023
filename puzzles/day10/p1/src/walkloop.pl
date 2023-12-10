5e727491508ca78bf665bb9b4d0da5cbd344cefd
Mike Fulton <mikefultonpersonal@gmail.com>
2023-12-10 17:19:24 -0500

    add an edge of dots around the edge

--- puzzles/day10/p1/src/walkloop.pl
+++ puzzles/day10/p1/src/walkloop.pl
@@ -3,11 +3,58 @@ use feature 'say';
 use warnings;
 use strict;
 
+# 
+# Symbols in the matrix.
+# Use global variables since the characters are confusing
+#
+
+our $v  = '|';
+our $ul = 'F';
+our $h  = '-';
+our $ur = '7';
+our $bl = 'L';
+our $br = 'J';
+our $s = 'S';
+
+#
+# Make the matrix global - this is a simple program
+#
+our @matrix;
+
+#
+# Make the starting point global
+#
+our $startcol = 0;
+our $startrow = 0;
+
+#
+# If I have 2 choices of up or down, go the direction I am currently going
+# If I have 2 choices of left or right, go the direction I am currently going
+# At the start, there will be 2 ways to being. Choose up over down and right over left
+#
 sub nextChoice {
   my ($row, $col) = @_;
 
   say "Next choice is [$row . $col]";
+
+  my $c = $matrix[$row][$col];
+
   return ($row, $col);
+}
+
+sub addLine {
+  my ($row, $line) = @_;
+  my $col = 0;
+
+  my @spl = split('', $line);
+  foreach my $i (@spl) {
+    $matrix[$row][$col] = $i;
+    if ($i eq $s) {
+      $startrow = $row;
+      $startcol = $col;
+    }
+    $col = $col+1;
+  } 
 }
 
 my $args = $#ARGV + 1;
@@ -21,48 +68,27 @@ my $input = $ARGV[0];
 
 open (FH, '<', $input) or die $!;
 
-our @matrix;
-
 my $row = 0;
 my $col = 0;
 
-my $startcol = 0;
-my $startrow = 0;
-
-my $s = 'S';
-
+my $edge = '';
 while (<FH>) {
   my $line = $_;
-  say "L: $line";
-  my @spl = split('', $line);
-  $col = 0;
-  foreach my $i (@spl) {
-    $matrix[$row][$col] = $i;
-    if ($i eq $s) {
-      $startrow = $row;
-      $startcol = $col;
-    }
-    $col = $col+1;
-  } 
+
+  if ($edge eq '') {
+    my $size = length($line);
+    $edge = '.' x $size;
+    addLine($row, $edge);
+    $row = $row+1;
+  }
+  # Add dots on the edges so I don't have to check for out of bounds in nextChoice
+  $line = ".$line."; 
+  addLine($row, $line);
   $row = $row+1;
 }
+addLine($row, $edge);
 
 say "Start point is [$startrow . $startcol]";
-
-# 
-# Check what direction I can go
-# Use variables since the characters are confusing
-#
-
-my $v  = '|';
-my $ul = 'F';
-my $h  = '-';
-my $ur = '7';
-my $bl = 'L';
-my $br = 'J';
-
-# If I have 2 choices of up or down, go the direction I am currently going
-# If I have 2 choices of left or right, go the direction I am currently going
 
 ($row, $col) = nextChoice($startrow, $startcol);
 
