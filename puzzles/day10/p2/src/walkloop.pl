8d6c2a0aabc438f1ec4c0633d6bdd2364f0db6ea
Mike Fulton <mikefultonpersonal@gmail.com>
2023-12-10 23:39:40 -0500

    got the answer

--- puzzles/day10/p2/src/walkloop.pl
+++ puzzles/day10/p2/src/walkloop.pl
@@ -40,30 +40,30 @@ our $startrow = 0;
 #
 # If I have 2 choices of up or down, go the direction I am currently going
 # If I have 2 choices of left or right, go the direction I am currently going
-# At the start, there will be 2 ways to being. Choose up over down and right over left
+# At the start, there will be 2 ways to start. Choose up over down and left over right
 #
 sub nextChoice {
-  my ($row, $col, $dir, $clean) = @_;
+  my ($row, $col, $dir, $mark) = @_;
 
   my $c = $matrix[$row][$col];
 
-  if ($dir eq $du || $dir eq $ds) {
-    my $up = $matrix[$row-1][$col];
-    if ($up eq $s) {
-      return ($row-1, $col, $de);
+  if ($dir eq $dr || $dir eq $ds) {
+    my $right = $matrix[$row][$col+1];
+    if ($right eq $s) {
+      return ($row, $col+1, $de);
     }
-    if (($up eq $v) || ($up eq $ul) || ($up eq $ur)) {
-      if ($up eq $v) {
-        $dir = $du;
-      } elsif ($up eq $ul) {
+    if (($right eq $h) || ($right eq $ur) || ($right eq $br)) {
+      if ($right eq $h) {
         $dir = $dr;
+      } elsif ($right eq $ur) {
+        $dir = $dd;
       } else {
-        $dir = $dl;
+        $dir = $du;
       }
-      if ($clean == 1 && $matrix[$row][$col-1] eq $x) {
-        $matrix[$row][$col-1] = $i;
+      if ($mark == 1 && $matrix[$row-1][$col] eq $x) {
+        $matrix[$row-1][$col] = $i;
       }
-      return ($row-1, $col, $dir);
+      return ($row, $col+1, $dir);
     }
   }
 
@@ -80,30 +80,30 @@ sub nextChoice {
       } else {
         $dir = $dl;
       }
-      if ($clean == 1 && $matrix[$row][$col+1] eq $x) {
+      if ($mark == 1 && $matrix[$row][$col+1] eq $x) {
         $matrix[$row][$col+1] = $i;
       }
       return ($row+1, $col, $dir);
     }
   }
 
-  if ($dir eq $dr || $dir eq $ds) {
-    my $right = $matrix[$row][$col+1];
-    if ($right eq $s) {
-      return ($row, $col+1, $de);
+  if ($dir eq $du || $dir eq $ds) {
+    my $up = $matrix[$row-1][$col];
+    if ($up eq $s) {
+      return ($row-1, $col, $de);
     }
-    if (($right eq $h) || ($right eq $ur) || ($right eq $br)) {
-      if ($right eq $h) {
-        $dir = $dr;
-      } elsif ($right eq $ur) {
-        $dir = $dd;
-      } else {
+    if (($up eq $v) || ($up eq $ul) || ($up eq $ur)) {
+      if ($up eq $v) {
         $dir = $du;
+      } elsif ($up eq $ul) {
+        $dir = $dr;
+      } else {
+        $dir = $dl;
       }
-      if ($clean == 1 && $matrix[$row-1][$col] eq $x) {
-        $matrix[$row-1][$col] = $i;
+      if ($mark == 1 && $matrix[$row][$col-1] eq $x) {
+        $matrix[$row][$col-1] = $i;
       }
-      return ($row, $col+1, $dir);
+      return ($row-1, $col, $dir);
     }
   }
 
@@ -120,7 +120,7 @@ sub nextChoice {
       } else {
         $dir = $du;
       }
-      if ($clean == 1 && $matrix[$row+1][$col] eq $x) {
+      if ($mark == 1 && $matrix[$row+1][$col] eq $x) {
         $matrix[$row+1][$col] = $i;
       }
       return ($row, $col-1, $dir);
@@ -168,8 +168,8 @@ sub otherPathChar {
 
 sub clearOriginalPathChar {
   my ($numrows, $numcols) = @_;
-  for (my $row = 0; $row < $numrows; ++$row) {
-    for (my $col = 0; $col < $numcols; ++$col) {
+  for (my $row = 1; $row < $numrows-1; ++$row) {
+    for (my $col = 1; $col < $numcols-1; ++$col) {
       my $c = $matrix[$row][$col];
 
       # Put dash ($h) at end so it does not get mis-interpreted as range
@@ -181,14 +181,59 @@ sub clearOriginalPathChar {
 
 sub switchPathChar {
   my ($numrows, $numcols) = @_;
-  for (my $row = 0; $row < $numrows; ++$row) {
-    for (my $col = 0; $col < $numcols; ++$col) {
+  for (my $row = 0; $row < $numrows-1; ++$row) {
+    for (my $col = 0; $col < $numcols-1; ++$col) {
       my $c = $matrix[$row][$col];
 
       $c = otherPathChar($c);
       $matrix[$row][$col] = $c;
     }
   }
+}
+
+sub expandMarks {
+  my $changed = 0;
+  my ($numrows, $numcols) = @_;
+  for (my $row = 1; $row < $numrows-2; ++$row) {
+    for (my $col = 1; $col < $numcols-2; ++$col) {
+      my $c = $matrix[$row][$col];
+
+      if ($c eq $i) {
+        # Check the 4 spots that touch
+        if ($matrix[$row-1][$col] eq $x) {
+          $changed = 1;
+          $matrix[$row-1][$col] = $i;
+        }
+        if ($matrix[$row+1][$col] eq $x) {
+          $changed = 1;
+          $matrix[$row+1][$col] = $i;
+        }
+        if ($matrix[$row][$col-1] eq $x) {
+          $changed = 1;
+          $matrix[$row][$col-1] = $i;
+        }
+        if ($matrix[$row][$col+1] eq $x) {
+          $changed = 1;
+          $matrix[$row][$col+1] = $i;
+        }
+      }
+    }
+  }
+  return $changed;
+}
+
+sub countMarks {
+  my ($numrows, $numcols, $markchar) = @_;
+  my $mark=0;
+  for (my $row = 0; $row < $numrows-1; ++$row) {
+    for (my $col = 0; $col < $numcols-1; ++$col) {
+      my $c = $matrix[$row][$col];
+      if ($c eq $markchar) {
+        $mark = $mark+1;
+      }
+    }
+  }
+  return $mark;
 }
 
 sub printMatrix {
@@ -234,6 +279,7 @@ while (<FH>) {
   addLine($row, $line);
   $row = $row+1;
 }
+close(FH);
 addLine($row, $edge);
 my $numrows = $row;
 
@@ -241,56 +287,78 @@ say "Start point is [$startrow . $startcol]";
 
 my $dir = $ds;
 
-my $clean = 0;
-($row, $col, $dir) = nextChoice($startrow, $startcol, $dir, $clean);
+my $mark = 0;
+($row, $col, $dir) = nextChoice($startrow, $startcol, $dir, $mark);
 
 my $count = 0;
 while ($matrix[$row][$col] ne $s) {
   $count = $count+1;
   my $prevrow = $row;
   my $prevcol = $col;
-  ($row, $col, $dir) = nextChoice($row, $col, $dir, $clean);
+  ($row, $col, $dir) = nextChoice($row, $col, $dir, $mark);
 
+  say "$row $col $dir";
   $matrix[$prevrow][$prevcol] = otherPathChar($matrix[$prevrow][$prevcol]);
 }
 
 my $farthest = $count/2;
 
-printMatrix();
+#printMatrix();
 
 #
 # Clear the broken pipes on the ground
 #
 clearOriginalPathChar($numrows, $numcols);
 
-printMatrix();
+#printMatrix();
 
 #
 # Switch the pipe characters back again
 #
 switchPathChar($numrows, $numcols);
 
-printMatrix();
+#printMatrix();
 
+#
+# Now that the broken pipes are cleared out, make another walk 
+# around the loop and this time mark all the empty spots on the 
+# inside right beside the path with a I ($i) for inside. 
+#
 $matrix[$startrow][$startcol] = $s;
 $dir = $ds;
-($row, $col, $dir) = nextChoice($startrow, $startcol, $dir, $clean);
-$clean = 1;
+($row, $col, $dir) = nextChoice($startrow, $startcol, $dir, $mark);
+$mark = 1;
 
 while ($matrix[$row][$col] ne $s) {
-  say "Next choice is [$row . $col . $dir]";
   $count = $count+1;
   my $prevrow = $row;
   my $prevcol = $col;
-  ($row, $col, $dir) = nextChoice($row, $col, $dir, $clean);
+  ($row, $col, $dir) = nextChoice($row, $col, $dir, $mark);
 }
 
 #
 # Switch the pipe characters back again
 #
 switchPathChar($numrows, $numcols);
+
+#printMatrix();
+#
+# Now we need to 'grow' the marked spots by converting all 
+# .'s the touch I's to into I's
+#
+
+say "About to expand marks";
+
+my $iter=0;
+while (my $changed = expandMarks($numrows, $numcols)) {
+  $iter = $iter+1;
+  say "Expand marks iteration: $iter";
+}
+
 printMatrix();
 
-say "Loop walk was $count steps. Farthest point away is $farthest";
+my $answerA = countMarks($numrows, $numcols, $i);
+my $answerB = countMarks($numrows, $numcols, $x);
 
-close(FH);
+say "Loop walk was $answerA and $answerB inside and outside tiles";
+
