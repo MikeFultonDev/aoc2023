85be6283e662e578ea965f6e7ae053abc0f1a5c6
Mike Fulton <mikefultonpersonal@gmail.com>
2023-12-12 00:00:14 -0500

    read file into array

--- puzzles/day11/p1/src/universe.groovy
+++ puzzles/day11/p1/src/universe.groovy
@@ -1,7 +1,39 @@
-
+if (args.length < 1) {
+  println("Syntax: universe <text file>")
+  System.exit(4)
+}
 String file = args[0]
 
+def cols=0
+def rows=0
+def col=0
+def row=0
+
+def matrix
+def lines = []
+
 new File(file).eachLine { line ->
-    println line
+  lines.add(line)
+}
+rows = lines.size()
+
+for (row=0; row<rows; ++row) {
+    def aLine = lines.get(row).split('')
+    if (cols == 0) {
+      cols = aLine.length
+      matrix = new String[rows][cols]
+    }
+    col = 0
+    for (String entry : aLine) {
+      matrix[row][col++] = entry;
+    }
 }
 
+println "rows: " + row + " cols: " + cols
+
+for (row = 0; row < rows; ++row) {
+  for (col = 0; col < cols; ++col) {
+    print matrix[row][col]
+  }
+  println ""
+}
