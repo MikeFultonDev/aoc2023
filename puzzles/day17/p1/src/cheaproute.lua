01ea68a9b666976b0d13b9b9d788e4f9138068f3
Mike Fulton <mikefultonpersonal@gmail.com>
2023-12-18 22:58:16 -0500

    basic simple code searching - gets wrong total

---
+++ puzzles/day17/p1/src/cheaproute.lua
@@ -0,0 +1,118 @@
+#!/bin/env lua
+-- 
+-- Find the cheapest route from upper-left to lower-right
+-- with conditions on how to traverse.
+--
+function create_matrix(lines)
+  local matrix = {}
+  local rows = #lines
+  for row = 1, rows do matrix[row] = {} end
+
+  for row, line in pairs(lines) do
+    cols = #line
+    for col = 1, cols do
+      local c = line:sub(col,col)
+      matrix[row][col] = c
+    end
+  end
+  return matrix
+end
+
+function print_matrix(matrix)
+  for row = 1, #matrix do
+    local line = ""
+    for col = 1, #matrix[row] do
+      local c = matrix[row][col]
+      line=line..c
+    end
+    print(line)
+  end
+end
+
+function calc_lowest_cost(matrix, directions, row, col, cost)
+  if cost > row*col*9 then return math.huge end
+
+  local max_same_dir = 3
+  if directions ~= nil and string.len(directions) >= max_same_dir then
+    local all_equal=true
+    local cur = string.sub(directions, -1)
+    local start = string.len(directions) - max_same_dir+1
+    local finis = string.len(directions) - 1
+    --print("Compare: "..string.sub(directions, start, finis).." to: "..cur)
+    for i = start, finis do
+      if string.sub(directions, i, i) ~= cur then 
+        --print("All equal false")
+        all_equal = false
+      end
+    end
+    if all_equal then 
+      --print("Directions: "..directions.." equal")
+      return math.huge 
+    end
+  end
+
+  if row == #matrix and col == #matrix[row] then 
+    print("One solution: Direction: "..directions.." with cost:"..cost)
+    return cost 
+  end
+
+  local calc = { math.huge, math.huge, math.huge, math.huge }
+  if col > 1 then 
+    --Left
+    calc[1] = calc_lowest_cost(matrix, directions.."L", row, col-1, cost+matrix[row][col-1])
+  end
+  if col < #matrix[row] then 
+    --Right
+    calc[2] = calc_lowest_cost(matrix, directions.."R", row, col+1, cost+matrix[row][col+1])
+  end
+  if row > 1 then 
+    --Up
+    calc[3] = calc_lowest_cost(matrix, directions.."U", row-1, col, cost+matrix[row-1][col])
+  end
+  if row < #matrix then 
+    --Down
+    calc[4] = calc_lowest_cost(matrix, directions.."D", row+1, col, cost+cost + matrix[row+1][col])
+  end
+  local min = math.huge
+  for i, c in pairs(calc) do
+    --print(c)
+    min = math.min(min, c)
+  end
+  return min 
+end
+
+function file_exists(file)
+  local f = io.open(file, "rb")
+  if f~=nil then io.close(f) return true else return false end
+end
+
+function lines_from(file)
+  if not file_exists(file) then return {} end
+  local lines = {}
+  for line in io.lines(file) do
+    lines[#lines + 1] = line
+  end
+  return lines
+end
+
+if #arg < 1 then
+  print("Syntax "..arg[-1].." <heatloss-map>")
+  return
+end
+
+lines = lines_from(arg[1])
+if #lines == 0 then 
+  print("File "..arg[1].." is empty or does not exist")
+  return
+end
+
+matrix = create_matrix(lines)
+print_matrix(matrix)
+
+-- The cheapest route will be no more than 9*nrows*ncols
+-- Use this as a high-water mark to avoid any infinite loops
+
+local directions = ""
+local cost = calc_lowest_cost(matrix, directions, 1, 1, 0)
+
+print("Lowest Cost "..cost) 
