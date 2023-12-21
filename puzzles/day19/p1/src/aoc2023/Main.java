d00b11aa26ea10b2904ae826eb08a8a52c4b6fa7
Mike Fulton <mikefultonpersonal@gmail.com>
2023-12-20 20:35:46 -0500

    parsed parts

--- puzzles/day19/p1/src/aoc2023/Main.java
+++ puzzles/day19/p1/src/aoc2023/Main.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.TreeSet;
 
 class Main {
-  public static void main(String args[]) {
+  public static void main(String args[]) throws java.io.IOException {
     if (args.length != 1) {
       System.out.printf("Syntax: java %s <workflow>\n", Main.class.getName());
       System.exit(4);
@@ -25,40 +25,37 @@ class Main {
     }
   }
 
-  Main(String workflowFile) {
+  Main(String workflowFile) throws java.io.IOException {
     this._workflowFile = workflowFile;
   }
 
-  private boolean processWorkflows() {
+  private boolean processWorkflows() throws java.io.IOException {
     ArrayList<String> workflowLines = new ArrayList<String>();
     ArrayList<String> partLines = new ArrayList<String>();
     ArrayList<Workflow> workflows = new ArrayList<Workflow>();
     ArrayList<Part> parts = new ArrayList<Part>();
     ArrayList<String> lines = workflowLines;
     ProcessingState state = ProcessingState.ProcessingWorkflows;
-    try {
-      BufferedReader br = new BufferedReader(new FileReader(this._workflowFile));
-      String l;
-      while ((l = br.readLine()) != null) {
-        String trimline = l.trim();
-        if (!trimline.equals("")) {
-          lines.add(trimline);
-        } else {
-          if (state == ProcessingState.ProcessingWorkflows) {
-            state = ProcessingState.ProcessingParts;
-            lines = partLines;
-          }
+
+    BufferedReader br = new BufferedReader(new FileReader(this._workflowFile));
+    String l;
+    while ((l = br.readLine()) != null) {
+      String trimline = l.trim();
+      if (!trimline.equals("")) {
+        lines.add(trimline);
+      } else {
+        if (state == ProcessingState.ProcessingWorkflows) {
+          state = ProcessingState.ProcessingParts;
+          lines = partLines;
         }
       }
-    } catch (IOException e) {
-      System.err.println("Error " + e + " encountered trying to process workflow file " + this._workflowFile);
-      return true;
     }
 
     System.out.println("Parts");
     for (String part : partLines) {
-      System.out.println(part);
-      parts.add(new Part(part));
+      Part p = new Part(part);
+      System.out.println(p);
+      parts.add(p);
     }
     System.out.println();
     System.out.println("Workflows");
--- puzzles/day19/p1/src/aoc2023/Part.java
+++ puzzles/day19/p1/src/aoc2023/Part.java
@@ -1,11 +1,67 @@
 package aoc2023;
 import java.util.List;
+import java.util.ArrayList;
+import java.io.StringReader;
+import java.io.Reader;
+import java.io.StreamTokenizer;
 
 class Part {
-  Part(String partLine) {
+  Part(String partLine) throws java.io.IOException {
     this._partLine = partLine;
+    parse();
+  }
+  private void parse() throws java.io.IOException {
+    Reader r = new StringReader(this._partLine);
+    StreamTokenizer st = new StreamTokenizer(r);
+    List<Object> tokens = new ArrayList<Object>();
+    double token;
+
+    // Sample line: {x=2127,m=1623,a=2188,s=1013}
+    // No error checking for invalid lines
+    String curVar = "";
+    double curVal;
+    while ((token = st.nextToken()) != StreamTokenizer.TT_EOF) {
+      if (st.ttype == StreamTokenizer.TT_WORD) {
+        curVar = st.sval;
+      } else if (st.ttype == StreamTokenizer.TT_NUMBER) {
+        curVal = st.nval;
+        if (curVar.equals("x")) {
+          this._x = curVal;
+        } else if (curVar.equals("m")) {
+          this._m = curVal;
+        } else if (curVar.equals("a")) {
+          this._a  = curVal;
+        } else if (curVar.equals("s")) {
+          this._s = curVal;
+        } else {
+          System.err.println("Unable to parse line: " + this._partLine);
+        }
+      }
+    }
   }
 
+  private double _x;
+  private double _m;
+  private double _a;
+  private double _s;
+
   private String _partLine;
+
+  public long x() {
+    return Double.valueOf(this._x).longValue();
+  }
+  public long m() {
+    return Double.valueOf(this._m).longValue();
+  }
+  public long a() {
+    return Double.valueOf(this._a).longValue();
+  }
+  public long s() {
+    return Double.valueOf(this._s).longValue();
+  }
+
+  public String toString() {
+    return "x: " + x() + " m: " + m() + " a: " + a() + " s: " + s();
+  }
 }
 
