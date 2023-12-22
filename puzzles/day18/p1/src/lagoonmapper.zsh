b0a58d4f6fb9ed0c94778642a8f3acb06a5738e3
Mike Fulton <mikefultonpersonal@gmail.com>
2023-12-22 02:04:25 +0000

    FINALLY found the problem with grid... Turned out it was because I was calling with $(...) syntax

--- puzzles/day18/p1/src/lagoonmapper.zsh
+++ puzzles/day18/p1/src/lagoonmapper.zsh
@@ -23,6 +23,30 @@ map_rawtext_to_commands()
 # https://stackoverflow.com/questions/16487258/how-to-declare-2d-array-in-bash
 #
 
+set_cell()
+{
+  local srow="$1"
+  local scol="$2"
+  local sc="$3"
+  local skey="${srow},${scol}"
+
+  grid["${skey}"]="$sc"
+  echo "set $srow $scol $sc" >&2
+}
+
+get_cell()
+{
+  local grow="$1"
+  local gcol="$2"
+  local gkey="${grow},${gcol}"
+  local gval=${grid[(k)"${gkey}"]}
+  if [ "x${gval}" = "x" ]; then 
+    gval="${EMPTY}"
+  fi
+  echo "get $grow $gcol -> $gval" >&2
+  return ${gval}
+}
+
 print_grid()
 {
   local startrow=$1
@@ -35,12 +59,20 @@ print_grid()
   while [ ${row} -le ${endrow} ]; do
     local col=$startcol
     while [ ${col} -le ${endcol} ]; do
-      local val=$(get_cell ${row} ${col})
-      if [ -z ${val} ]; then
-        printf "%s" '?'
-      else
-        printf "%s" "${val}"
-      fi
+      get_cell "${row}" "${col}"
+      val=$?
+      case $val in
+        $DUG)
+          sval='#'
+          ;;
+        $EMPTY)
+          sval='.'
+          ;;
+        $OUT)
+          sval='O'
+          ;;
+      esac
+      printf "%s" "${sval}"
       col=$((col+1))
     done
     row=$((row+1))
@@ -60,37 +92,14 @@ fill_in_grid()
   while [ ${row} -le ${endrow} ]; do
     local col=$startcol
     while [ ${col} -le ${endcol} ]; do
-      local val=$(get_cell ${row} ${col})
-      if [ -z ${val} ]; then
-        set_cell ${row} ${col} "." 
+      get_cell "${row}" "${col}"
+      if [ $? -eq $EMPTY ]; then
+        set_cell "${row}" "${col}" "${EMPTY}"
       fi
       col=$((col+1))
     done
     row=$((row+1))
   done
-}
-
-set_cell()
-{
-  local srow=$1
-  local scol=$2
-  local sc=$3
-  local skey="${srow},${scol}"
-
-  grid["${skey}"]="$sc"
-  #local sval=${grid[(k)"${skey}"]}
-  #echo "set: cell[$key] is ${sval}" >&2
-}
-
-get_cell()
-{
-  local grow=$1
-  local gcol=$2
-  local gkey="${grow},${gcol}"
-  local gval=${grid[(k)"${gkey}"]}
-  #echo "get: cell[$gkey] is ${gval}" >&2
-  echo "${gval}"
-  return 0
 }
 
 set_neighbour_dots()
@@ -99,47 +108,46 @@ set_neighbour_dots()
   local startcol=$2
   local endrow=$3
   local endcol=$4
-  local row=$5
-  local col=$6
-  local out=$7
-  local context="nochange"
+  local nrow=$5
+  local ncol=$6
 
-  local lcol=$((col-1))
-  local rcol=$((col+1))
-  local lrow=$((row-1))
-  local rrow=$((row+1))
-  local c
-  c=$(get_cell ${row} ${lcol})
-  if [ "$c" = "." ] ; then
-    set_cell ${row} ${lcol} "${out}"
-    context="update"
+  local context=${NOCHANGE}
+
+  local lcol=$((ncol-1))
+  local rcol=$((ncol+1))
+  local lrow=$((nrow-1))
+  local rrow=$((nrow+1))
+  local cval 
+
+set -x
+  get_cell "${nrow}" "${lcol}"
+  cval=$?
+  if [ ${cval} -eq ${EMPTY} ] ; then
+    set_cell "${nrow}" "${lcol}" "${OUT}"
+    context=${UPDATE}
   fi
-  c=$(get_cell ${row} ${rcol})
-  if [ "$c" = "." ] ; then
-    set_cell ${row} ${rcol} "${out}"
-    context="update"
+  get_cell "${nrow}" "${rcol}"
+  cval=$?
+  if [ ${cval} -eq ${EMPTY} ] ; then
+    set_cell "${nrow}" "${rcol}" "${OUT}"
+    context=${UPDATE}
   fi
-  c=$(get_cell ${lrow} ${col})
-  if [ "$c" = "." ] ; then
-    set_cell ${lrow} ${col} "${out}"
-    context="update"
+  get_cell "${lrow}" "${ncol}"
+  cval=$?
+  if [ ${cval} -eq ${EMPTY} ] ; then
+    set_cell "${lrow}" "${ncol}" "${OUT}"
+    context=${UPDATE}
   fi
-  c=$(get_cell ${rrow} ${col})
-  if [ "$c" = "." ] ; then
-    set_cell ${rrow} ${col} "${out}"
-    context="update"
+  get_cell "${rrow}" "${ncol}"
+  cval=$?
+  if [ ${cval} -eq ${EMPTY} ] ; then
+    set_cell "${rrow}" "${ncol}" "${OUT}"
+    context=${UPDATE}
   fi
-local key=''
-local value=''
-echo "Type of grid: ${(t)grid}" >&2
-for key value in ${(kv@)grid}; do
-  echo "$key -> $value" >&2
-done
-exit 0
+set +x
 
   #echo "cell[$row][$col] ${context}" >&2
-  echo "${context}"
-  return 0
+  return ${context}
 }
 
 flood_fill()
@@ -148,37 +156,37 @@ flood_fill()
   local startcol=$2
   local endrow=$3
   local endcol=$4
-  local out="O"
 
-  print_grid ${startrow} ${startcol} ${endrow} ${endcol}
-  set_cell ${startrow} ${startcol} ${out}
-  set_cell ${startrow} ${endcol} ${out}
-  set_cell ${endrow} ${startcol} ${out}
-  set_cell ${endrow} ${endcol} ${out}
+  print_grid "${startrow}" "${startcol}" "${endrow}" "${endcol}"
+  set_cell "${startrow}" "${startcol}" "${OUT}"
+  set_cell "${startrow}" "${endcol}" "${OUT}"
+  set_cell "${endrow}" "${startcol}" "${OUT}"
+  set_cell "${endrow}" "${endcol}" "${OUT}"
 
-  local context="update"
+  typeset -p grid >&2
+
+  local context=${UPDATE}
   local iters=0
-  while [ "${context}" = "update" ]; do
-    print_grid ${startrow} ${startcol} ${endrow} ${endcol}
+  while [ ${context} -eq ${UPDATE} ]; do
+    typeset -p grid >&2
+    print_grid "${startrow}" "${startcol}" "${endrow}" "${endcol}" 
     echo "Iterate on flood"
     iters=$((iters+1))
     if [ $iters -gt 3 ]; then 
       return 0
     fi
-    context="nochange"
+    context=${NOCHANGE}
     local row=$startrow
     while [ ${row} -le ${endrow} ]; do
       local col=$startcol
       while [ ${col} -le ${endcol} ]; do
-        local val=$(get_cell ${row} ${col})
-        if [ -z ${val} ]; then
-          echo "All cells should be set now" >&2
-        else
-          if [ "${val}" = "${out}" ]; then
-            local thiscontext=$(set_neighbour_dots ${startrow} ${startcol} ${endrow} ${endcol} ${row} ${col} "${out}")
-            if [ "${thiscontext}" = "update" ]; then
-              context="update"
-            fi
+        get_cell "${row}" "${col}"
+        val=$?
+        if [ ${val} -eq ${OUT} ]; then
+          set_neighbour_dots "${startrow}" "${startcol}" "${endrow}" "${endcol}" "${row}" "${col}"
+          thiscontext=$?
+          if [ ${thiscontext} -eq ${UPDATE} ]; then
+            context=${UPDATE}
           fi
         fi
         col=$((col+1))
@@ -193,13 +201,13 @@ update_horizontal()
   local row=$1
   local startcol=$2
   local length=$3
-  local c=$4
   local count=0
   while [ ${count} -lt ${length} ]; do
     local col=$((startcol+count))
-    local val=$(get_cell ${row} ${col})
-    if [ -z ${val} ]; then
-      set_cell ${row} ${col} "$c"
+    get_cell "${row}" "${col}"
+    val=$?
+    if [ ${val} -eq ${EMPTY} ]; then
+      set_cell "${row}" "${col}" "${DUG}"
     fi
     count=$((count+1))
   done
@@ -210,13 +218,13 @@ update_vertical()
   local col=$1
   local startrow=$2
   local length=$3
-  local c=$4
   local count=0
   while [ ${count} -lt ${length} ]; do
     local row=$((startrow+count))
-    local val=$(get_cell ${row} ${col})
-    if [ -z ${val} ]; then
-      set_cell ${row} ${col} "$c"
+    get_cell "${row}" "${col}"
+    val=$?
+    if [ ${val} -eq ${EMPTY} ]; then
+      set_cell "${row}" "${col}" "${DUG}"
     fi
     count=$((count+1))
   done
@@ -234,19 +242,19 @@ excavate()
   
   case ${direction} in
     R)
-      update_horizontal ${row} $((col+1)) ${length} '#'
+      update_horizontal ${row} $((col+1)) ${length}
       col=$((col+length))
       ;;
     L)
-      update_horizontal ${row} $((col-length)) ${length} '#'
+      update_horizontal ${row} $((col-length)) ${length}
       col=$((col-length))
       ;;
     D)
-      update_vertical ${col} $((row+1)) ${length} '#'
+      update_vertical ${col} $((row+1)) ${length}
       row=$((row+length))
       ;;
     U)
-      update_vertical ${col} $((row-length)) ${length} '#'
+      update_vertical ${col} $((row-length)) ${length}
       row=$((row-length))
       ;;
   esac
@@ -296,7 +304,15 @@ fi
 #
 col=1000
 row=1000
-set_cell ${row} ${col} "#"
+
+DUG=1
+EMPTY=2
+OUT=3
+
+UPDATE=1
+NOCHANGE=2
+
+set_cell "${row}" "${col}" "${DUG}"
 
 maxcol=0
 maxrow=0
@@ -317,9 +333,9 @@ done
 
 echo "Range: ${minrow},${mincol} to ${maxrow},${maxcol}"
 
-fill_in_grid ${minrow} ${mincol} ${maxrow} ${maxcol}
+#fill_in_grid "${minrow}" "${mincol}" "${maxrow}" "${maxcol}" "${EMPTY}"
 
-print_grid ${minrow} ${mincol} ${maxrow} ${maxcol}
+print_grid "${minrow}" "${mincol}" "${maxrow}" "${maxcol}"
 
 #
 # The dig lines cross over each other so safe approach
@@ -327,6 +343,6 @@ print_grid ${minrow} ${mincol} ${maxrow} ${maxcol}
 # (blank edge was inserted to ensure this is 'safe')
 #
 
-flood_fill ${minrow} ${mincol} ${maxrow} ${maxcol}
+flood_fill "${minrow}" "${mincol}" "${maxrow}" "${maxcol}"
 
-print_grid ${minrow} ${mincol} ${maxrow} ${maxcol}
+print_grid "${minrow}" "${mincol}" "${maxrow}" "${maxcol}"
--- puzzles/day18/p1/src/tester.zsh
+++ puzzles/day18/p1/src/tester.zsh
@@ -1,43 +1,75 @@
 #!/bin/env zsh
-set_grid() 
+
+set_cell()
+{
+  local srow=$1
+  local scol=$2
+  local sc=$3
+  local skey="${srow},${scol}"
+
+  grid["${skey}"]="$sc"
+  echo "set $srow $scol $sc" >&2
+  return $sc
+}
+
+get_cell()
+{
+  local grow=$1
+  local gcol=$2
+  local gkey="${grow},${gcol}"
+  local gval=${grid[(k)"${gkey}"]}
+  echo "get $grow $gcol $gval" >&2
+  echo "${gval}"
+  return $gval
+}
+
+set_deepest() 
 {
   local row=$1
   local col=$2
-  local val=$3
-  local key="${row},${col}"
-  grid["${key}"]="${val}"
+  x=5
+  typeset -p grid >&2
+  set_cell "${row}" "${col}" "2"
+  val=$?
+  typeset -p grid >&2
+  return $val
 }
 
+set_deeper()
+{
+  local row=$1
+  local col=$2
+  x=4
+  col=$((col+1))
+  row=$((row+1))
+  typeset -p grid >&2
+  y=$(set_deepest "$row" "$col")
+  val=$?
+  return $val
+}
+
+set -x
 declare -A grid
 
-col=1
-row=1
-set_grid $col $row '#'
-col=$((col+1))
-row=$((row+1))
-set_grid $col $row '#'
+v=$(get_cell "1010" "1008")
+echo "$v"
 
-col=4
-entry=${grid[(k)"$col,$row"]}
+set_cell "1010" "1008" "1"
+v=$(get_cell "1010" "1008")
+echo "$v"
 
-if [ -z ${entry} ]; then
-  echo 'empty'
-fi
-echo "$entry"
+v=$(get_cell "1010" "1008")
+echo "$v"
 
-col=7
-row=7
-set_grid $col $row 'A'
-set_grid $col $row 'A'
-row=8
-set_grid $col $row 'B'
-row=7
-set_grid $col $row 'A'
-set_grid $col $row 'A'
-set_grid $col $row 'A'
-set_grid $col $row 'A'
-set_grid $col $row 'A'
+row=1009
+col=1007
+export x=2
+echo $x
+q=$(set_deeper "$row" "$col")
+val=$?
+echo $val
+echo $x
 
-for key value in ${(kv)grid}; do
-  echo "$key -> $value"
-done
+v=$(get_cell "1010" "1008")
+  typeset -p grid >&2
+echo $v
